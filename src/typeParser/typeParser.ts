726710ec39cbcd3d62aee1e79921e0b653edc5cc
garebareDA <garedaiki@gmail.com>
2025-08-16 18:00:33 +0900

    typeParserに型の決定関数を追加

--- src/typeParser/typeParser.ts
+++ src/typeParser/typeParser.ts
@@ -2,9 +2,16 @@ export function typeParser(types: string) {
   return types.split('|').map((type) => type.trim());
 }
 
-export function isIncludedType(
-  array: ReadonlyArray<string>,
-  type: ReadonlyArray<string>
-): boolean {
-  return array.some((value) => type.includes(value));
+export function firstIntersectionType(
+  inputTypes: ReadonlyArray<string>,
+  supportedTypes: ReadonlyArray<string>
+): string | null {
+  const set = new Set(supportedTypes);
+  for (const value of inputTypes) {
+    if (set.has(value)) {
+      return value;
+    }
+  }
+
+  throw new Error(`Not Supported Type ${inputTypes.join(', ')}`);
 }
